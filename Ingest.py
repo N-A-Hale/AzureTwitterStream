b1246c1112c16b72d4957392a73e0e3a658163a3
Nicholas Hale <102883685+N-A-Hale@users.noreply.github.com>
2022-07-29 13:23:01 +1000

    Update: Ingest.py

--- Ingest.py
+++ Ingest.py
@@ -16,16 +16,53 @@ import json
 # COMMAND ----------
 
 # Twitter Auth
+
 bearer_token = dbutils.secrets.get(scope = "TwitterStreamKV", key = "BearerToken")
-auth = { "Authotization" : f"Bearer {bearer_token}", "User-Agent" : "v2FilteredStreamPython"}
+auth = { "Authorization" : f"Bearer {bearer_token}", "User-Agent" : "v2FilteredStreamPython"}
 
 # COMMAND ----------
 
 # Filtered Stream access
+
 rules = [ { "value" : "(#UkraineWare OR #WARINUKRAINE) -is:retweet lang:en", "tag" : "#UkraineWar tag" } ]
 payload = { "add" : rules }
 response = requests.post("https://api.twitter.com/2/tweets/search/stream/rules",
                          headers = auth, json = payload)
+
+# COMMAND ----------
+
+# Connecting to Event Hub
+
+eventhub_policy = dbutils.secrets.get(scope = "TwitterStreamKV", key = "EventHubPolicy")
+eventhub_name = dbutils.secrets.get(scope = "TwitterStreamKV", key = "EventHubName")
+eventhub_namespace = dbutils.secrets.get(scope = "TwitterStreamKV", key = "EventHubNamespace")
+eventhub_sas = dbutils.secrets.get(scope="TwitterStreamKV", key = "EventHubSAS")
+ 
+shared_key_credential = EventHubSharedKeyCredential(eventhub_policy,eventhub_sas)
+eh_producer_client = EventHubProducerClient(fully_qualified_namespace=eventhub_namespace, credential=shared_key_credential, eventhub_name=eventhub_name)
+
+
+# COMMAND ----------
+
+response = requests.get(
+    "https://api.twitter.com/2/tweets/search/stream?expansions=author_id&tweet.fields=created_at,public_metrics", headers = auth, stream=True,
+)
+ 
+if response.status_code != 200:
+    raise Exception(
+        "Cannot get stream (HTTP {}): {}".format(
+            response.status_code, response.text
+        )
+    )
+increment =  0
+for response_line in response.iter_lines():
+    if response_line:
+        increment += 1
+        print(f"Message {increment} received")
+        event_data_batch = ehpc.create_batch()
+        data = EventData(body=response_line)
+        event_data_batch.add(data)
+        ehpc.send_batch(event_data_batch)
 
 # COMMAND ----------
 
