3d7363ee078b73468170115412a85a6957aea6b1
Nicholas Hale <102883685+N-A-Hale@users.noreply.github.com>
2022-07-29 12:28:20 +1000

    Update: Ingest.py

--- Ingest.py
+++ Ingest.py
@@ -1,1 +1,12 @@
+# Databricks notebook source
+# MAGIC %pip install azure-eventhub
 
+# COMMAND ----------
+
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from pyspark.sql import *
+import datetime
+import requests
+import os
+import json
