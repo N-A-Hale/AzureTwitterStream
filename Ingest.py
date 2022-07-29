de326625ab3c542951804885eb791de8e99d1bba
Nicholas Hale <102883685+N-A-Hale@users.noreply.github.com>
2022-07-29 13:10:34 +1000

    Update: Ingest.py

--- Ingest.py
+++ Ingest.py
@@ -6,7 +6,26 @@
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark.sql import *
+from azure.eventhub import EventData, EventHubProducerClient, EventHubSharedKeyCredential
 import datetime
 import requests
 import os
 import json
+
+
+# COMMAND ----------
+
+# Twitter Auth
+bearer_token = dbutils.secrets.get(scope = "TwitterStreamKV", key = "BearerToken")
+auth = { "Authotization" : f"Bearer {bearer_token}", "User-Agent" : "v2FilteredStreamPython"}
+
+# COMMAND ----------
+
+# Filtered Stream access
+rules = [ { "value" : "(#UkraineWare OR #WARINUKRAINE) -is:retweet lang:en", "tag" : "#UkraineWar tag" } ]
+payload = { "add" : rules }
+response = requests.post("https://api.twitter.com/2/tweets/search/stream/rules",
+                         headers = auth, json = payload)
+
+# COMMAND ----------
+
